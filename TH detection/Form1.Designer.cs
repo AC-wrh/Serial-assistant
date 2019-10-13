00545724deca15a7783ddeee8d73a601b6df074d
adolchristin <adolchristin.wrh@gmail.com>
2019-10-13 21:22:37 +0800

    Initial commit

---
+++ TH detection/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace TH_detection
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(584, 58);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(407, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 29);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "开始检测";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(224, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 29);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.Location = new System.Drawing.Point(160, 15);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 21);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "串口：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(55, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "无";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(55, 7);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "无";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(10, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "湿度：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(10, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "温度：";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.chart1);
+            this.panel2.Location = new System.Drawing.Point(12, 64);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(560, 268);
+            this.panel2.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series1.Legend = "Legend1";
+            series1.Name = "温度";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Time;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(556, 264);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel3.Controls.Add(this.chart2);
+            this.panel3.Location = new System.Drawing.Point(12, 338);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(560, 268);
+            this.panel3.TabIndex = 2;
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(0, 0);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series2.Legend = "Legend1";
+            series2.Name = "湿度";
+            series2.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Time;
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(556, 264);
+            this.chart2.TabIndex = 0;
+            this.chart2.Text = "chart2";
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel4.Controls.Add(this.label6);
+            this.panel4.Controls.Add(this.label5);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(0, 611);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(584, 30);
+            this.panel4.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(84, 2);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(183, 21);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "yyyy/mm/dd hh:mm:ss";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(3, 2);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 21);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "当前时间：";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.SerialPort1_DataReceived);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 641);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel4);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+		private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+		private System.IO.Ports.SerialPort serialPort1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Timer timer2;
+	}
+}
+
---
+++ TH detection/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace TH_detection
+{
+    public partial class Form1 : Form
+	{
+		private StringBuilder sb = new StringBuilder();     //为了避免在接收处理函数中反复调用，依然声明为一个全局变量
+		private StringBuilder builder = new StringBuilder();    //避免在事件处理方法中反复创建，定义为全局
+
+		public class MonitoringData     //声明一个存放检测数据的类
+		{
+			public static int T = 0, H = 0;     //定义两个静态变量,T-温度 H-湿度
+		}
+
+		Random rnd = new Random();      //随机数（测试用）---> rnd.Next(500)
+
+		public Form1()
+		{
+			InitializeComponent();  //初始化窗口
+			serialPort1.BaudRate = 9600;    
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			comboBox1.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());    //获取电脑当前可用串口并添加到选项列表中
+		}
+
+		/*******************************************************************************
+		* 函数名		：button1_Click
+		* 函数功能		：“打开串口”按钮点击事件
+		* 输入			：无
+		* 输出			：无
+		* 作者			：ChristinAdol
+		*******************************************************************************/
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				//将可能产生异常的代码放置在try块中
+				//根据当前串口属性来判断是否打开
+				if (serialPort1.IsOpen)
+				{
+					//串口已经处于打开状态
+					serialPort1.Close();    //关闭串口
+					button1.Text = "开始检测";
+					comboBox1.Enabled = true;
+					timer1.Stop();
+					label3.Text = "无";
+					label4.Text = "无";
+				}
+				else
+				{
+					//串口已经处于关闭状态，则设置好串口属性后打开
+					comboBox1.Enabled = false;
+					serialPort1.PortName = comboBox1.Text;
+					serialPort1.BaudRate = 9600;
+					serialPort1.DataBits = 8;
+					serialPort1.Parity   = System.IO.Ports.Parity.None;
+					serialPort1.StopBits = System.IO.Ports.StopBits.One;
+
+					serialPort1.Open();     //打开串口
+					button1.Text = "停止检测";
+					timer1.Start();
+				}
+			}
+			catch (Exception ex)        //捕获可能发生的异常并进行处理
+			{
+				//捕获到异常，创建一个新的对象，之前的不可以再用
+				serialPort1 = new System.IO.Ports.SerialPort();
+				//刷新COM口选项
+				comboBox1.Items.Clear();
+				comboBox1.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());
+				//响铃并显示异常给用户
+				System.Media.SystemSounds.Beep.Play();
+				button1.Text = "开始检测";
+				MessageBox.Show(ex.Message);
+				comboBox1.Enabled = true;
+
+			}
+		}
+
+		/*******************************************************************************
+		* 函数名		：SerialPort1_DataReceived
+		* 函数功能		：串口接收处理程序
+		* 输入			：无
+		* 输出			：无
+		* 作者			：ChristinAdol
+		*******************************************************************************/
+		private void SerialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+		{
+
+			int num = serialPort1.BytesToRead;      //获取接收缓冲区中的字节数
+			byte[] received_buf = new byte[num];    //声明一个大小为num的字节数据用于存放读出的byte型数据
+			serialPort1.Read(received_buf, 0, num);   //读取接收缓冲区中num个字节到byte数组中
+
+			sb.Clear();     //防止出错,首先清空字符串构造器
+			sb.Append(Encoding.ASCII.GetString(received_buf));
+
+			if (num != 0)
+			{
+				MonitoringData.T = sb[0];
+				MonitoringData.H = sb[1];
+			}
+
+
+			try
+			{
+				//因为要访问UI资源，所以需要使用invoke方式同步ui
+				Invoke((EventHandler)(delegate
+				{
+					label3.Text = (MonitoringData.T.ToString() + " ℃");
+					label4.Text = (MonitoringData.H.ToString() + " %");
+				}
+				  )
+				);
+			}
+			catch (Exception ex)
+			{
+				//响铃并显示异常给用户
+				System.Media.SystemSounds.Beep.Play();
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		/*******************************************************************************
+		* 函数名		：AddNewPoint()
+		* 函数功能		：为chart添加新的点
+		* 输入			：timeStamp、ptSeries、value
+		* 输出			：无
+		* 作者			：ChristinAdol
+		*******************************************************************************/
+		private void AddNewPoint(DateTime timeStamp, Series ptSeries, int value)
+		{
+			//将新数据点添加到其系列中
+			ptSeries.Points.AddXY(timeStamp.ToOADate(), value);
+
+			double removePoint = timeStamp.AddMinutes((double)(9) * (-1)).ToOADate();      //设定当前时间9min前的点
+
+			while (ptSeries.Points[0].XValue < removePoint)
+			{
+				ptSeries.Points.RemoveAt(0);    //删除超过当前时间9min前的点。
+			}
+			//设置 chart1 x轴时间值
+			chart1.ChartAreas[0].AxisX.Minimum = ptSeries.Points[0].XValue;     
+			chart1.ChartAreas[0].AxisX.Maximum = DateTime.FromOADate(ptSeries.Points[0].XValue).AddMinutes(10).ToOADate();    
+			//设置 chart2 x轴时间值
+			chart2.ChartAreas[0].AxisX.Minimum = ptSeries.Points[0].XValue;     
+			chart2.ChartAreas[0].AxisX.Maximum = DateTime.FromOADate(ptSeries.Points[0].XValue).AddMinutes(10).ToOADate();    
+
+			//chart1.Invalidate();
+			//chart2.Invalidate();
+		}
+
+		/*******************************************************************************
+		* 函数名		：timer1_Tick
+		* 函数功能		：定时调用描点函数
+		* 输入			：无
+		* 输出			：无
+		* 作者			：ChristinAdol
+		*******************************************************************************/
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			DateTime timeStamp = DateTime.Now;      //获取当前时间到timeStamp
+			AddNewPoint(timeStamp, chart1.Series[0], MonitoringData.T);
+			AddNewPoint(timeStamp, chart2.Series[0], MonitoringData.H);
+		}
+
+		/*******************************************************************************
+		* 函数名		：timer2_Tick
+		* 函数功能		：定时更新显示时间
+		* 输入			：无
+		* 输出			：无
+		* 作者			：ChristinAdol
+		*******************************************************************************/
+		private void timer2_Tick(object sender, EventArgs e)
+		{
+			label6.Text = System.DateTime.Now.ToString();
+		}
+	}
+}
